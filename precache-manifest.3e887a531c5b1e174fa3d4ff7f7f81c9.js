bcb35f9f6ffbc1e6ca22e5fe2fc02cd5455be5de
João Gomes <joaosilgo96@gmail.com>
2020-10-21 03:59:36 +0100

    Updates

---
+++ precache-manifest.3e887a531c5b1e174fa3d4ff7f7f81c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd07c8da20c3e4e1403051aac5ce829",
+    "url": "/john-package-test/index.html"
+  },
+  {
+    "revision": "ddd259ef7327efcfaef6",
+    "url": "/john-package-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9b06574f549339cb450",
+    "url": "/john-package-test/static/js/2.42d39179.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/john-package-test/static/js/2.42d39179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd259ef7327efcfaef6",
+    "url": "/john-package-test/static/js/main.72d717c9.chunk.js"
+  },
+  {
+    "revision": "9158aade643ae853ee10",
+    "url": "/john-package-test/static/js/runtime-main.7798957e.js"
+  }
+]);
